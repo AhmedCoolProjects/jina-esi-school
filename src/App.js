8b56e75a56da24c291d9ca9b6248c498024f7a5e
Ahmed BARGADY <bargadyahmed2000@gmail.com.com>
2021-02-23 01:01:43 +0100

    finished the TopBottomBarNav.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
+import TopBottomBarNav__ from "./app/components/TopBottomBarNav__";
+import Dashboard_ from "./app/pages/Dashboard_";
 import "./base.css";
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <TopBottomBarNav__ />
+      <Dashboard_ />
+    </div>
+  );
 }
 
 export default App;
